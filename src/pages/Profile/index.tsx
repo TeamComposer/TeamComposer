9436fe67023840fed4d69b1c4b94f085d7fcddee
LuisBertuol <luisbertuol57@gmail.com>
2024-05-27 19:14:37 -0300

    Update index.tsx

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity } from 'react-native';
 
 function Profile({ navigation }) {
-  //teste
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
