d03521ce64dab53030c5f4d3ce2a3e3e49d04ab8
CarlosViinicius <carlosvendramini51@gmail.com>
2024-06-16 22:04:25 -0300

    Profile Screen.

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,5 +1,4 @@
 import React, { useContext, useEffect, useState } from "react";
-
 import {
   Text,
   View,
@@ -8,7 +7,7 @@ import {
   ScrollView,
   ImageBackground,
 } from "react-native";
-import { FontAwesome, MaterialIcons } from "@expo/vector-icons"; // Importando o ícone
+import { MaterialIcons } from "@expo/vector-icons";
 import styles from "./styles";
 import { Context as UserContext } from "../../context/userContext";
 
@@ -34,19 +33,12 @@ const Profile = () => {
           style={styles.profileImage}
         />
         <View style={styles.profileDetails}>
-          <View style={styles.stars}>
-            {Array(5)
-              .fill(null)
-              .map((_, index) => (
-                <FontAwesome
-                  key={index}
-                  name="star"
-                  size={20}
-                  color="#FFD700"
-                  style={styles.star}
-                />
-              ))}
-          </View>
+          <Text style={styles.name}>
+            {infoUser.primeiroNome + " " + infoUser.sobrenome}{" "}
+          </Text>
+          <Text style={styles.profession}>
+            <Text style={styles.bullet}>• </Text>{stateUser.team?.nome ? stateUser.team.nome : 'Sem time'}
+          </Text>
         </View>
         <TouchableOpacity
           style={styles.menuButton}
@@ -56,78 +48,6 @@ const Profile = () => {
         >
           <MaterialIcons name="more-vert" size={30} color="white" />
         </TouchableOpacity>
-      </View>
-      <View style={styles.nameContainer}>
-        <Text style={styles.name}>
-          {infoUser.primeiroNome + " " + infoUser.sobrenome}{" "}
-        </Text>
-        <Text style={styles.profession}>
-          <Text style={styles.bullet}>• </Text>{stateUser.team?.nome ? stateUser.team.nome : 'Sem time'}
-        </Text>
-      </View>
-      <View style={styles.footer}>
-        <View style={styles.body}>
-          <Text style={styles.description}>
-            Olá! Sou a {infoUser.primeiroNome + " " + infoUser.sobrenome}, estudante do 6º período de Engenharia de
-            Software e profissional de UX/UI. Estou sempre em busca de novos
-            desafios e oportunidades para aprimorar minhas habilidades. Com uma
-            paixão pela experiência do usuário e um background técnico sólido,
-            estou animada para aplicar meus conhecimentos em projetos criativos
-            e inovadores. Estou aberta a colaborações e pronta para contribuir
-            para o sucesso de projetos de desenvolvimento de software. Vamos
-            criar experiências incríveis juntos!
-          </Text>
-        </View>
-        <View style={styles.socialContainer}>
-          <TouchableOpacity
-            style={styles.socialButton}
-            accessible
-            accessibilityLabel="Discord"
-          >
-            <Image
-              source={{
-                uri: "https://freelogopng.com/images/all_img/1691730813discord-icon-png.png",
-              }}
-              style={styles.socialIcon}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.socialButton}
-            accessible
-            accessibilityLabel="WhatsApp"
-          >
-            <Image
-              source={{
-                uri: "https://freelogopng.com/images/all_img/1661938251whatsapp-logo-png.png",
-              }}
-              style={styles.socialIcon}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.socialButton}
-            accessible
-            accessibilityLabel="Instagram"
-          >
-            <Image
-              source={{
-                uri: "https://freelogopng.com/images/all_img/1658587303instagram-png.png",
-              }}
-              style={styles.socialIcon}
-            />
-          </TouchableOpacity>
-          <TouchableOpacity
-            style={styles.socialButton}
-            accessible
-            accessibilityLabel="LinkedIn"
-          >
-            <Image
-              source={{
-                uri: "https://freelogopng.com/images/all_img/1656996409linkedin-symbol.png",
-              }}
-              style={styles.socialIcon}
-            />
-          </TouchableOpacity>
-        </View>
       </View>
     </ScrollView>
   );
